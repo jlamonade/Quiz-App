a012a3cc2ae2eab1717d49dad25963a998f51aea
Jason Lam <nyjasonlam@gmail.com>
2021-04-16 17:01:29 -0400

    Added rough outline of different elements sets to show for each mode (play, enter high score, high scores)

---
+++ assets/scripts/script.js
@@ -0,0 +1,56 @@
+// Dependencies (HTML Elements, Libraries, etc.) =====================
+// Starting Data
+
+
+
+// Build
+
+
+// Starting Data =====================================================
+    // high scores are read from external data
+    // questions/answers
+
+// Functions =========================================================
+
+
+// User Interactions =================================================
+    // start button is clicked
+        // start button disappears
+        // game will start
+            // timer starts
+            // questions appear
+            // answers appear
+            // user clicks on an answer
+            // record outcome/decrement time
+            // show next question
+        // when timer runs out
+            // display user score
+            // record user score to high scores
+            // display high scores
+            // go back to start button
+
+
+// Initialization ====================================================
+
+
+// page loads 
+
+// show view highscores button
+    // when view highscores is pressed
+        // display high scores
+// show start button
+    // when start button is clicked
+        // timer starts
+        // questions and answers appear
+        // user chooses answer
+            // if correct answer is chosen
+                // alert user that they were correct
+                // record correct answer
+            // else if incorrect answer is chosen
+                // alert user that they were incorrect
+                // record incorrect answer
+                // take time away from timer
+        // if timer runs out
+            // display user score
+            // ask for their initials to record high scores
+            // go back to start button
